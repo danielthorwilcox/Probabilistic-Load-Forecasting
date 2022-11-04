797869940f29fbbce9ad7e2559c2bd25ec587d77
Leo Grosse <leonhardgrosse@gmx.de>
2022-11-04 23:57:09 +0100

    data cleaning: added resource for more interpolation techniques

--- interpolation.py
+++ interpolation.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#see https://towardsdatascience.com/imputing-missing-data-with-simple-and-advanced-techniques-f5c7b157fb87 for more techniques
+
 data = pd.read_csv("demand_generation/energy_dataset.csv")
 data.drop(["generation hydro pumped storage aggregated","forecast wind offshore eday ahead"],axis=1,inplace=True)
 linear_interpolation = data.interpolate(method='linear')
