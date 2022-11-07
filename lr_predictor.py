39ede17bbe0a689eff120b7ed534f8d2a1e4858f
Leo Grosse <leonhardgrosse@gmx.de>
2022-11-07 23:15:35 +0100

    updated linear regression to be trained on windows with look-back window and specific prediction distance

--- lr_predictor.py
+++ lr_predictor.py
@@ -1,45 +1,57 @@
+#-------------------------------------------------------------------------------------------------------------------
 #linear regression predictor for energy data
-#ToDo: move this to FORECASTING: right now, using the detailed load distribution as features makes the regression perform too well
-#in reality, this should be LR time-series forecasting: taking lagged 'total load actual' data, it should predict the next (n) values
+#divides data into observation and prediction windows, then uses a linear regression model to predict the values in the
+#prediction window based on the values in the corresponding observation window
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import numpy as np
 
-def calculate_metrics(df):
-    return {'mae' : mean_absolute_error(df["total load actual"], df.prediction),
-            'rmse' : mean_squared_error(df["total load actual"], df.prediction) ** 0.5,
-            'r2' : r2_score(df["total load actual"], df.prediction)}
 
-def feature_label_split(df, target_col):
-    y = df[[target_col]]
-    X = df.drop(columns=[target_col])
+def getXypairs(data, train_period, pred_period):
+    data.drop(columns='time', inplace=True)
+    n_observations = len(data)
+    window_size = train_period + pred_period
+    X = np.zeros([n_observations - window_size, train_period])
+    y = np.zeros([n_observations - window_size, pred_period])
+
+    for idx, x in enumerate(X):
+        X[idx, :] = data.iloc[idx:idx + train_period].to_numpy()
+        y[idx, :] = data.iloc[idx + train_period:idx + window_size].to_numpy()
+
     return X, y
 
-def build_baseline_model(df, test_ratio, target_col):
-    X, y = feature_label_split(df, target_col="total load actual")
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_ratio, shuffle=False
-    )
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    prediction = model.predict(X_test)
+#-----------------------------------------------------------------------------------
+# W_obs: number of old values used for prediction, W_pred: number of future values to predict  
 
-    print("coeffs: ", model.coef_)
-
-    result = pd.DataFrame(y_test)
-    result["prediction"] = prediction
-    result = result.sort_index()
-
-    return result
+W_obs = 24*20
+W_pred = 24*2  
 
 load_data = pd.read_csv("demand_generation/energy_dataset_lininterp.csv")
-data_nostamp = load_data.drop(columns="time",inplace=True)
+timeseries = load_data["total load actual"]
+print(timeseries.head())
 
-df_baseline = build_baseline_model(load_data, 0.2, 'value')
-baseline_metrics = calculate_metrics(df_baseline)
-print(baseline_metrics)
-#df_baseline.plot(title="Linear Regression predictions")
-#plt.show()
+X, y = getXypairs(timeseries,W_obs,W_pred)
+print(X.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,shuffle=False)
+
+lrmodel = LinearRegression()
+lrmodel.fit(X_train,y_train)
+predictions = lrmodel.predict(X_test)
+
+mse = mean_squared_error(y_test,predictions)
+r2 = r2_score(y_test,predictions)
+
+print("Linear Regression predictor with MSE: ", mse, ", r2 score: ", r2)
+#print("model coefficients: ", lrmodel.coef_)
+
+#'''
+plt.plot(y_test[400,:])
+plt.plot(predictions[400,:])
+plt.legend(["true values", "predictions"])
+plt.show()
+#'''
