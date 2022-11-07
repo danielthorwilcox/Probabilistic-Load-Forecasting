2ea06c300a41ef0baf88a0b9e85ad42def2132e5
Leo Grosse <leonhardgrosse@gmx.de>
2022-11-07 11:54:28 +0100

    added moving average predictor skeletton

---
+++ mwa_predictor.py
@@ -0,0 +1,11 @@
+#moving average predictor for energy data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+load_data = pd.read_csv("demand_generation/energy_dataset_lininterp.csv")
+#print(load_data.tail())
+load_data["SMA"] = load_data["total load actual"].rolling(12).mean()
+print(load_data.tail())
+load_data[["total load actual", "SMA"]].plot()
+plt.show()
